0144a8853a332726bf46a9117e8bda79ed180582
alentejo <arthur.alentejo228@gmail.com>
2024-09-11 00:45:52 -0300

    refact: user controller to fast api

--- src/v3/controller/user.py
+++ src/v3/controller/user.py
@@ -1,56 +1,62 @@
-from flask_restplus import Resource, Namespace, fields
-from flask import request
+from fastapi import APIRouter, Request, status
 from loguru import logger
-from datetime import datetime
 from base import Response, Services
-from domain.message import Message
+from v3.schemas.user_schema import UserSchema
 
-api = Namespace('User',description='User related operations')
-
-userModel = api.model('UserModel', {
-    'name': fields.String,
-    'password': fields.String
-})
+router = APIRouter(tags=["User"])
 
 services = Services()
 
-@api.route('/')
-class UserController(Resource):
-    @api.expect(userModel)
-    def post(self):
-        try:                       
-            name = request.json.get('name')
-            password = request.json.get('password')
-            
-            if name is None or len(name) == 0:
-                return Response.create_error_response(400, 'Empty name')
-                       
-            if password is None or len(password) == 0:
-                return Response.create_error_response(400, 'Empty password')
-            
-            id = self.user_service.create(name, password)
-            return Response.create_response(201, 'User created', {'id': id})
-                        
-        except Exception as e:
-            logger.error(f'Error creating user: {e}')
-            return Response.create_response(500, str(e))
 
-@api.route('/<id>')
-class UserIdController(Resource):        
-    def get(self, id):
-        try:
-            user_id = services.auth(request)
-            
-            if user_id is None or len(user_id) == 0:
-                return Response.create_error_response(401, 'Unauthorized')
-            
-            user = services.user_service.get(id)
-            if user is None:
-                return Response.create_error_response(404, 'User not found')
-            
-            return Response.create_response(200, 'User loaded', { 'user': user.ToJson()} )
-        
-        except Exception as e:
-            logger.error(f'Error getting user: {e}')
-            return Response.create_response(500, str(e))
-        
+@router.post("/")
+async def create_user(user_data: UserSchema):
+    try:
+        if not user_data.name:
+            return Response.create_error_response(
+                status=status.HTTP_400_BAD_REQUEST, error="Empty name"
+            )
+
+        if not user_data.password:
+            return Response.create_error_response(
+                status=status.HTTP_400_BAD_REQUEST, error="Empty password"
+            )
+
+        user_id = services.user_service.create(user_data.name, user_data.password)
+        return Response.create_response(
+            status=status.HTTP_201_CREATED, message="User created", data={"id": user_id}
+        )
+
+    except Exception as e:
+        logger.error(f"Error creating user: {e}")
+        return Response.create_error_response(
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR, error=str(e)
+        )
+
+
+@router.get("/{id}")
+async def get_user(id: int, request: Request):
+    try:
+        user_id = services.auth(request)
+
+        if not user_id:
+            return Response.create_error_response(
+                status=status.HTTP_401_UNAUTHORIZED, error="Unauthorized"
+            )
+
+        user = services.user_service.get(id)
+        if not user:
+            return Response.create_error_response(
+                status=status.HTTP_404_NOT_FOUND, error="User not found"
+            )
+
+        return Response.create_response(
+            status=status.HTTP_200_OK,
+            message="User loaded",
+            data={"user": user.ToJson()},
+        )
+
+    except Exception as e:
+        logger.error(f"Error getting user: {e}")
+        return Response.create_error_response(
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR, error=str(e)
+        )
