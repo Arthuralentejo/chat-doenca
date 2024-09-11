55881a40a150f56ce869029618ba700e911dad89
alentejo <arthur.alentejo228@gmail.com>
2024-09-11 00:45:31 -0300

    refact: auth controler to fast api

--- src/v3/controller/auth.py
+++ src/v3/controller/auth.py
@@ -1,48 +1,51 @@
-from flask_restplus import Resource, Namespace, fields
-from flask import request
+from fastapi import APIRouter, HTTPException, Request, status
 from loguru import logger
-from datetime import datetime
 from base import Response, Services
-from domain.message import Message
+from v3.schemas.auth_schema import AuthSchema
 
-api = Namespace('Auth', description='Auth related operations')
+router = APIRouter(
+    tags="Auth"
+)
 
-authModel = api.model('AuthModel', {
-    'password': fields.String
-})
 
 services = Services()
 
-@api.route('/<id>')
-class AuthController(Resource):
-    @api.expect(authModel)
-    def post(self, id: int):
-        try:
-            if id is None or len(id) == 0:
-                return Response.create_error_response(400, 'Empty id')
-                                    
-            password = request.json.get('password')
-                                   
-            if password is None or len(password) == 0:
-                return Response.create_error_response(400, 'Empty password')
-            
-            token = services.auth_service.login(id, password)
-            return Response.create_response(201, 'User login', {'token': token})
-                        
-        except Exception as e:
-            logger.error(f'Error to login user {id}: {e}')
-            return Response.create_response(500, str(e))
-        
-    def delete(self, id: int):
-        try:
-            if id is None or len(id) == 0:
-                return Response.create_error_response(400, 'Empty id')
-                                    
-            if services.auth_service.logout(id):
-                return Response.create_response(201, 'User logout', {'result': True})
-            else:
-                return Response.create_error_response(404, 'User not found')
-                        
-        except Exception as e:
-            logger.error(f'Error to login user {id}: {e}')
-            return Response.create_response(500, str(e))        
+
+@router.post("/{id}")
+async def login_user(id: int, auth_data: AuthSchema, request: Request):
+    try:
+        if not id:
+            Response.create_error_response(
+                status=status.HTTP_400_BAD_REQUEST, error="Empty id"
+            )
+
+        if not auth_data.password:
+            Response.create_error_response(
+                status=status.HTTP_400_BAD_REQUEST, error="Empty password"
+            )
+
+        token = services.auth_service.login(id, auth_data.password)
+        return Response.create_response(201, "User login", {"token": token})
+
+    except Exception as e:
+        logger.error(f"Error to login user {id}: {e}")
+        Response.create_error_response(status=500, error=str(e))
+
+
+@router.delete("/{id}")
+async def logout_user(id: int, request: Request):
+    try:
+        if not id:
+            Response.create_error_response(
+                status=status.HTTP_400_BAD_REQUEST, error="Empty id"
+            )
+
+        if services.auth_service.logout(id):
+            return Response.create_response(201, "User logout", {"result": True})
+        else:
+            Response.create_error_response(
+                status=status.HTTP_404_NOT_FOUND, error="User not found"
+            )
+    except Exception as e:
+        logger.error(f"Error to logout user {id}: {e}")
+        Response.create_error_response(status=500, error=str(e))
